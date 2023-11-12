635f393cf410a7344e16437cae9032ade09da216
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-12 21:44:17 +0100

    updates

--- custom_commands_win/runserver.bat
+++ custom_commands_win/runserver.bat
@@ -1,9 +1,16 @@
 @echo off
 
+rem pull latest updates
+echo .................... Pulling latest updates ....................
+git pull
+echo ...............................................................
+
 rem Starts the local server
 echo .................... Starting local server ....................
 echo Use CTRL + C to quit the server
 echo ...............................................................
+
+rem cd C:\Users\pc\
 
 python manage.py runserver
 
