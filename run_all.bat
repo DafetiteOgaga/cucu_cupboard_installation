507dd0256254a5e0792d2d5e4d1e57e18da51664
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-21 10:44:59 +0100

    updates

--- run_all.bat
+++ run_all.bat
@@ -8,13 +8,5 @@ call set_env_var.bat
 cd %dir%
 call cucu_cupboard_install.bat
 
-set /P "resp=Do you want to run the program now? [y/N] >>> "
-
-if /i "%resp%"=="y" if not "%resp%"=="" (
-	call "%dir%\custom_commands_win\runserver_all.bat"
-) else (
-	echo .
-	echo To run the program, run: runserver_all
-	echo .
-    exit /b 0
-)
+echo Now, close this window.
+echo Open another window and run: runserver_all
