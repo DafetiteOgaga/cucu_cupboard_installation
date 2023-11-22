f8fded1756a64370f9cf338c186148557c70a309
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-22 14:42:23 +0100

    updates

--- custom_commands_win/runserver_all.bat
+++ custom_commands_win/runserver_all.bat
@@ -3,25 +3,25 @@
 set "repo_path=C:\Users\%username%\xbin_windows"
 echo ...................................................0
 echo Pulling latest updates ...
-call "%repo_path%runserver_pull.bat"
+call "%repo_path%\runserver_pull.bat"
 
 echo ...................................................1
 echo Pushing changes to remote ...
-call "%repo_path%runserver_push.bat"
+call "%repo_path%\runserver_push.bat"
 
 echo ...................................................2
 echo Activating virtual environment ...
-call "%repo_path%runserver_activate_venv.bat"
+call "%repo_path%\runserver_activate_venv.bat"
 
 echo ...................................................3
 echo Updating dependencies ...
-call "%repo_path%runserver_install_dependencies.bat"
+call "%repo_path%\runserver_install_dependencies.bat"
 
 echo ...................................................4
 echo Updating scripts ...
-call "%repo_path%runserer_copy.bat"
+call "%repo_path%\runserer_copy.bat"
 
 echo ...................................................5
 echo ............ Starting local server ............
-call "%repo_path%runserver_start_local_server.bat"
+call "%repo_path%\runserver_start_local_server.bat"
 echo ...............................................................
